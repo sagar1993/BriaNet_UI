fe1e48057e0a8e613e53a8165ab37f810dfb7fb8
Sagar1993 <patni.sagar12@gmail.com>
2017-11-21 19:39:10 -0700

    initial commit

---
+++ app/src/main/java/com/example/born_2_code/brainnet1/IntentServiceConnectFog.java
@@ -0,0 +1,27 @@
+/*
+package com.example.born_2_code.brainnet1;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+*/
+/**
+ * Created by Born_2_Code on 11/20/2017.
+ *//*
+
+
+
+public class IntentServiceConnectFog extends IntentService {
+
+
+    public IntentServiceConnectFog(){
+
+    }
+
+    @Override
+    protected void onHandleIntent(Intent workIntent) {
+
+        String dataString = workIntent.getDataString();
+
+    }
+}*/
